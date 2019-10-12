01f7fa871c8ff88c9deb6f8d8fc64c1f909ef6b1
banyobe <banyobe@esat-alumni.com>
2019-10-12 18:11:13 +0200

    New attributes for a GameObject: Material, Transform and Geometry.... READ COMMENTS

--- include/game_object.h
+++ include/game_object.h
@@ -1,5 +1,56 @@
+/*
+ * Author: Pablo Bano Benito <banyobe@esat-alumni.com>
+ * Date: 10-12-2019
+ * GameObject Header (Material, Transform, Geometry)
+ */
+
 #ifndef __GAME_OBJECT_H__
 #define __GAME_OBJECT_H__
+
+#include <glm.hpp>
+#include <data_types.h>
+
+// --------------------------------------------------- //
+
+struct Transform {
+	glm::vec3 scale;
+	glm::vec3 position;
+	glm::vec3 rotation;
+};
+
+// --------------------------------------------------- //
+
+class Material /*: public Referenced*/ {
+
+// This constructor will be protected when we add the ref_ptr class
+public:
+	Material() {};
+	~Material() {};
+
+public:
+	// Methods
+};
+
+// --------------------------------------------------- //
+
+class Geometry /*: public Referenced*/ {
+
+// This constructor will be protected when we add the ref_ptr class
+public:
+	Geometry() {};
+	~Geometry() {};
+
+private:
+
+	// Attributes
+	u16 number_elements_;
+	u16 indices_;
+
+	// Methods
+
+};
+
+// --------------------------------------------------- //
 
 class GameObject /*: public Referenced*/ {
 
@@ -8,9 +59,21 @@ public:
 	GameObject();
 	~GameObject();
 
-	// 
+private:
+	// Attributes
+	// TODO: This attributes will be a ref_ptr Template (constructor protected)
+	Material material_;
+	Geometry geometry_;
+
+	Transform transform_;
+
+
+	// Methods
+	// TODO: Call to DISPLAY_LIST add
 	void Draw();
 
 };
+
+// --------------------------------------------------- //
 
 #endif // __GAME_OBJECT_H__
