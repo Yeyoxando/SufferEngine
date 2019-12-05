7358934a9edd5f689f21dfd366e04d18aebf38fb
banyobe <banyobe@esat-alumni.com>
2019-12-05 23:31:25 +0100

    Updated with Crossfade feature

--- src/commands/audio_commands.cc
+++ src/commands/audio_commands.cc
@@ -1,5 +1,20 @@
+/*
+* Author: Pablo Bano Benito <banyobe@esat-alumni.com>
+* Date: 11-10-2019
+* AudioCommands Source
+*/
 
 #include <audio_commands.h>
+#include "thread.h"
+#include "ref_ptr.h"
+#include "math_utils.h"
+#include "common_definitions.h"
+#include "suffermanager.h"
+
+Suffer::Audio3D* Suffer::AudioCommands::Crossfade::from_;
+Suffer::Audio3D* Suffer::AudioCommands::Crossfade::to_;
+float Suffer::AudioCommands::Crossfade::attenuation_;
+bool Suffer::AudioCommands::Crossfade::ended_;
 
 // --------------------------------------------------------------//
 
@@ -38,3 +53,56 @@ void Suffer::AudioCommands::SetGain::Execute() const {
 
 }
 
+Suffer::AudioCommands::Crossfade::Crossfade(Audio3D* from, Audio3D* to, float attenuation){
+  from_ = from;
+  to_ = to;
+  attenuation_ = ThiefUtils::Math::Clamp(attenuation, 0.0f, 1.0f);
+  ended_ = false;
+}
+
+void Suffer::AudioCommands::Crossfade::Execute() const{
+
+#ifdef CUSTOM
+
+  ref_ptr<Thread> coroutine_;
+  coroutine_.alloc();
+  
+  auto crossfade_job = [] { Crossfading(); };
+  coroutine_->NewTask(crossfade_job);
+
+#else
+  
+  from_->Fade(0.0f, 2);
+  to_->Fade(1.0f, 2);
+
+#endif
+
+}
+
+void Suffer::AudioCommands::Crossfade::Crossfading(){
+
+#ifdef CUSTOM
+
+  suffer.audio_manager_.crossfade_mutex_.lock();
+  ended_ = false;
+  
+  while (!ended_) {
+  
+    float current_from_gain_ = from_->GetGain();
+    current_from_gain_ = ThiefUtils::Math::Lerp(current_from_gain_, 0.0f, attenuation_);
+    from_->SetGain(current_from_gain_);
+  
+    float current_to_gain_ = to_->GetGain();
+    current_to_gain_ = ThiefUtils::Math::Lerp(current_to_gain_, 1.0f, attenuation_);
+    to_->SetGain(current_to_gain_);
+    
+    if (from_->GetGain() <= 0.01f && to_->GetGain() >= 0.97f) ended_ = true;
+    else ended_ = false;
+  
+  };
+  
+  suffer.audio_manager_.crossfade_mutex_.unlock();
+
+#endif
+
+}
