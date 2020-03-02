7cfc8dfa628481109e824dd432190169f759904e
Neverwinter2 <itsneverwinter2@gmail.com>
2020-03-02 13:11:38 +0100

    Changed the usage of Radians insted of Degrees in the scripts

--- src/lua/lua_test_update_2.lua
+++ src/lua/lua_test_update_2.lua
@@ -10,8 +10,8 @@ local rotation = 0
 local speed = 0
 function Update()
   --print "Updating CUBE from LUA!"
-  rotation = rotation + 0.03
-  speed = speed + 0.002
+  rotation = rotation + 0.01
+  if rotation>=(2 * 3.141592) then rotation = 0 end
   Rotate(rotation, 0.0, rotation);
   --Translate(math.sin(os.clock()) * 20.0 + speed, 
   --          math.cos(os.clock()) * 20.0 + speed, 
